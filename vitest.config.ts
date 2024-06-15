c04a889e2755288c4c4392fa495343927ab7d1d2
nickap <49733176+nickap@users.noreply.github.com>
2024-06-15 16:25:43 +0300

    Chore: Vitest added. Migrated to new Vite setup 🛠️

---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import { fileURLToPath } from 'node:url';
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
+import viteConfig from './vite.config';
+
+const mergedViteConfig = viteConfig({
+  command: 'serve',
+  mode: 'development'
+});
+
+export default mergeConfig(
+  mergedViteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/**'],
+      root: fileURLToPath(new URL('./', import.meta.url))
+    }
+  })
+);
